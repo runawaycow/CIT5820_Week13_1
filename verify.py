8861e1ecc92076e17a70148673d5b76baef88ace
Dan Luo <luo_dana@yahoo.com>
2023-04-17 22:59:15 +1000

    Update verify.py

--- verify.py
+++ verify.py
@@ -7,7 +7,7 @@ def signChallenge( challenge ):
     w3 = Web3()
 
     #This is the only line you need to modify
-    sk = 0
+    sk = 0xd85ac92e6103e1e21f2fa0c771e2fa4d655bd8e8
     
 
     acct = w3.eth.account.from_key(sk)
@@ -40,46 +40,3 @@ if __name__ == '__main__':
         print( f"You passed the challenge!" )
     else:
         print( f"You failed the challenge!" )
-
-from web3 import Web3
-from eth_account.messages import encode_defunct
-import random
-
-def signChallenge( challenge ):
-
-    w3 = Web3()
-
-    #This is the only line you need to modify
-    sk = "YOUR SECRET KEY HERE"
-
-    acct = w3.eth.account.from_key(sk)
-
-    signed_message = w3.eth.account.sign_message( challenge, private_key = acct._private_key )
-
-    return acct.address, signed_message.signature
-
-
-def verifySig():
-    """
-        This is essentially the code that the autograder will use to test signChallenge
-        We've added it here for testing 
-    """
-
-    challenge_bytes = random.randbytes(32)
-
-    challenge = encode_defunct(challenge_bytes)
-    address, sig = signChallenge( challenge )
-
-    w3 = Web3()
-
-    return w3.eth.account.recover_message( challenge , signature=sig ) == address
-
-if __name__ == '__main__':
-    """
-        Test your function
-    """
-    if verifySig():
-        print( f"You passed the challenge!" )
-    else:
-        print( f"You failed the challenge!" )
-
