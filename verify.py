609f0f57ce620ad126319f2666243efed03a2fe1
Dan Luo <luo_dana@yahoo.com>
2023-04-17 23:12:01 +1000

    Update verify.py

--- verify.py
+++ verify.py
@@ -9,35 +9,9 @@ def signChallenge( challenge ):
     w3 = Web3()
 
 
-    # Connect to the Ethereum network
-    w3 = Web3(Web3.HTTPProvider('https://api.avax-test.network/ext/bc/C/rpc'))
-
-    # Address of the wallet that claimed the NFT
-    wallet_address = '0xd85ac92e6103e1e21f2fa0c771e2fa4d655bd8e8'
-
-    # Transaction hash of the NFT claim transaction
-    claim_tx_hash = '0x92a2d47452c6c9eaeb65159068251a69da138a4a219828dc2c871ee4abbce352'
-
-    # Get the transaction receipt for the NFT claim transaction
-    tx_receipt = w3.eth.getTransactionReceipt(claim_tx_hash)
-
-    # Get the contract address from the transaction receipt
-    contract_address = tx_receipt['0x85ac2e065d4526FBeE6a2253389669a12318A412']
-
-    # Load the NFT contract ABI
-    with open('nft_contract_abi.json', 'r') as f:
-        nft_contract_abi = json.load(f)
-
-    # Create a contract instance for the NFT contract
-    nft_contract = w3.eth.contract(address=contract_address, abi=nft_contract_abi)
-
-    # Get the token ID of the claimed NFT
-    token_id = nft_contract.functions.tokenOfOwnerByIndex(wallet_address, 0).call()
-
-    # Get the secret key for the wallet address
-    private_key = w3.eth.account.decrypt(keystore_json, 'password')
+    
     #This is the only line you need to modify
-    sk = private_key 
+    sk = 'bff1e88a649e5125d96928f35efc4f29a0b0785f3c05452c656fd79f49fe1dbd'
     
 
     acct = w3.eth.account.from_key(sk)
