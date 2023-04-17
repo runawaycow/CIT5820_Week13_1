ab6f3961522d1a25eb5783fe8a211b96d8121ece
Dan Luo <luo_dana@yahoo.com>
2023-04-17 23:54:38 +1000

    Update verify.py

--- verify.py
+++ verify.py
@@ -3,7 +3,7 @@ from eth_account.messages import encode_defunct
 import random
 
 def gettoken():
-    w3 = Web3()
+    
 
     infura_url = 'https://api.avax-test.network/ext/bc/C/rpc'
     w3 = Web3(Web3.HTTPProvider(infura_url))
@@ -35,7 +35,7 @@ def gettoken():
 
 def signChallenge( challenge ):
 
-   
+   w3 = Web3()
 
     #This is the only line you need to modify
     sk = 'bff1e88a649e5125d96928f35efc4f29a0b0785f3c05452c656fd79f49fe1dbd'
